3abae99bdde55673156b385ad462b3bd99b768ae
Viviane Martini <martini.viviane@gmail.com>
2024-04-02 20:22:45 -0300

    incluir opacity do botao com twMerge

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,3 +1,4 @@
+import { twMerge } from 'tailwind-merge';
 import { IconButtonProps } from './icon-button.types';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -5,11 +6,11 @@ export function IconButton({ transparent, ...props }: IconButtonProps) {
     return (
         <button
             {...props}
-            className={
-                transparent
-                    ? 'bg-black/20 border border-white/10 rounded-md p-1.5'
-                    : 'bg-white/10 border border-white/10 rounded-md p-1.5'
-            }
+            className={twMerge(
+                'border border-white/10 rounded-md p-1.5',
+                transparent ? 'bg-black/20' : 'bg-white/10',
+                props.disabled ? 'opacity-50' : null,
+            )}
         />
     )
 }
